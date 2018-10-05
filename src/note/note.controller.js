5765cfb9d34d5982e6048dafc7f3076e81527d73
Davina Leong <davina.leong.sy@gmail.com>
2018-10-05 22:38:12 +0800

    Implemented CRUD note functions

--- src/note/note.controller.js
+++ src/note/note.controller.js
@@ -8,7 +8,7 @@ module.exports = {
         })
         .catch(err => {
             res.send(500).send({
-                message: err.message || "An error occurred while retrieving all notes."
+                message: err.message || "An error occurred while retrieving all Notes."
             });
         });
     },
@@ -26,18 +26,85 @@ module.exports = {
             content: req.body.content,
             authorId: req.body.authorId || null
         });
+
+        note.save()
+            .then(result => {
+                res.send(result);
+            })
+            .catch(err => {
+                return res.status(500).send({
+                    message: "An error occurred while creating a Note."
+                });
+            });
     },
 
     find: (req, res) => {
         //TODO:  Find a note by its ID
+        Note.findById(req.params.noteId)
+            .then(note => {
+                if (! note) {
+                    return res.status(404).send({
+                        message: `Note not found.`
+                    });
+                }
+
+                res.send(note);
+            })
+            .catch(err => {
+                return res.status(500).send({
+                    message: "An error occurred while retrieving a Note."
+                });
+            });
     },
 
     update: (req, res) => {
-        //TODO: Update a note by its ID
         //TODO: Backend validation
+        if (! req.body.content) {
+            return res.status(404).send({
+                message: "The content field is required."
+            });
+        }
+
+        model.findByIdAndUpdate(req.params.noteId, {
+            title: req.body.title || "Untitled Note",
+            content: req.body.content,
+            authorId: req.body.authorId || null
+        }, { new: true })
+            .then(note => {
+                if (! note) {
+                    return res.status(404).send({
+                        message: "Note not found."
+                    });
+                }
+                res.send(note);
+            })
+            .catch(err => {
+                if (err.kind === 'ObjectId') {
+                    return res.status(404).send({
+                        message: "Note not found"
+                    });
+                }
+
+                res.status(500).send({
+                    message: err.message || "An error occurred while updating a Note."
+                });
+            });
     },
 
     delete: (req, res) => {
-        //TODO: Delete a note by its ID
+        note.findByIdAndDelete(req.params.noteId)
+            .then(note => {
+                if (! note) {
+                    return res.status(404).send({
+                        message: 'Note not found'
+                    });
+                }
+                res.send({ message: "Note deleted." });
+            })
+            .catch(err => {
+                return res.status(404).send({
+                    message: err.message || "An error occurred while deleting a Note."
+                });
+            });
     }
 };
