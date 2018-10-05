cb62eabaefca6344b311c44e7992d6f350671c1b
Davina Leong <davina.leong.sy@gmail.com>
2018-10-05 21:53:19 +0800

    Revert "Reorganised folder structure"
    This reverts commit 2245e63d9ea1f72f19fcb59159778b31712f11bb.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser    = require('body-parser');
 const mongoose      = require('mongoose');
 const path          = require('path');
 
-const config        = require('./lib/config');
+const config        = require('./config');
 /* #endregion */
 
 const app = express();
